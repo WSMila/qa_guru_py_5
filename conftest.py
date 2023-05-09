e3d134f058c6e29464567a2c93881b9e919ac517
gladkov@ukr.net <1Ama2deus#>
2023-05-09 13:49:34 +0300

    add hw for less 5

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ def broser_managment():
     chrome_options = webdriver.ChromeOptions()
     # chrome_options.add_argument('--headless')
     browser.config.driver_options = chrome_options
-    browser.config.window_width = 1400
-    browser.config.window_height = 1200
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
     browser.config.base_url = 'https://demoqa.com'
     # browser.config.timeout = 2.0
--- test_hw_5_student_reg_form.py
+++ test_hw_5_student_reg_form.py
@@ -1,4 +1,4 @@
-from selene import browser, have, by
+from selene import browser, have, by, be
 
 
 def test_student_reg_form():
@@ -10,16 +10,37 @@ def test_student_reg_form():
     browser.element('#lastName').set_value('Last Name')
     browser.element('#userEmail').set_value('test@e.mail')
     browser.element('#gender-radio-1').double_click()
-    browser.element('#userNumber').set_value('+1456321458')
+    browser.element('#userNumber').set_value('14563214580')
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__month-select').click()
     browser.all('.react-datepicker__month-select option').element_by(have.exact_text('July')).click()
     browser.element('.react-datepicker__year-select').click()
     browser.all('.react-datepicker__year-select option').element_by(have.exact_text('1980')).click()
     browser.all('.react-datepicker__day').element_by(have.exact_text('15')).click()
-    browser.element('#subjectsInput').type('Computer Sciense').press_tab()
-    browser.element('#hobbies-checkbox-1').click()
+    browser.element('[for="hobbies-checkbox-1"]').click()
+    browser.element('#subjectsInput').type('Co').press_enter()
+    browser.element('#uploadPicture').send_keys('D:/test.txt')
+    browser.element('#currentAddress').type('Green forest str.')
+    browser.element('#state').click()
+    browser.all('#state div').element_by(have.exact_text('Haryana')).click()
+    browser.element('#city').click()
+    browser.all('#city div').element_by(have.exact_text('Karnal')).click()
+    browser.element('#submit').click()
 
+    browser.element('.modal-content').should(be.visible)
+    browser.element('#example-modal-sizes-title-lg').should(have.exact_text('Thanks for submitting the form'))
+    browser.all('.table').all('td').should(have.exact_texts(
+        ("Student Name", "First Name Last Name"),
+        ("Student Email", "test@e.mail"),
+        ("Gender", "Male"),
+        ("Mobile", "1456321458"),
+        ("Date of Birth", "15 July,1980"),
+        ("Subjects", "Computer Science"),
+        ("Hobbies", "Sports"),
+        ("Picture", "test.txt"),
+        ("Address", "Green forest str."),
+        ("State and City", "Haryana Karnal")
+    ))
 
-    ...
-
+    browser.element('#closeLargeModal').click()
+    browser.should(have.url('https://demoqa.com/automation-practice-form'))
